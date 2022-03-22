6e1c3107f6ee89652ea604ec8c7b17e892a223b5
rajon <mostofarajon111@gmail.com>
2022-03-22 21:25:15 +0600

    order calculation added

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+    // console.log(cart);
+    let total =0;
+    let shipping =0;
+    for(const product of cart){
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = ((total *10) / 100).toFixed(2);
+    const grandTotal = total + shipping +parseFloat(tax)
     return (
-        <div>
+        <div className='cart'>
             <h3>Cart Order Summary</h3>
-            <p>Selected:{cart.length}</p>
+            <p>Selected Items:{cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax:{tax}</p>
+            <p>Grand Total:{grandTotal.toFixed(2)}</p>
         </div>
     );
 };
